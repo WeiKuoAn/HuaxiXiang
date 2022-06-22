7d038ec4d639a448f8b80150ac56e21ab9ca24e1
WeiKuoAn <qaz898970@gmail.com>
2022-06-22 16:49:33 +0800

    2022-6-22

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -31,7 +31,10 @@ class WorkController extends Controller
         $sale_today = Sale::where('sale_date',$today->format("Y-m-d"))->whereIn('pay_id',['A','B','C'])->count();
         $price = Sale::where('sale_date',$today->format("Y-m-d"))->whereIn('pay_id',['A','B','C'])->sum('pay_price');
         //月營收
-        $price_month = Sale::where('sale_date','>=',$firstDay->format("Y-m-d"))->where('sale_date','<=',$lastDay->format("Y-m-d"))->sum('pay_price');
+        $sale_month = Sale::where('sale_date','>=',$firstDay->format("Y-m-d"))->where('sale_date','<=',$lastDay->format("Y-m-d"))->sum('pay_price');
+        $income_month = IncomeData::where('income_date','>=',$firstDay->format("Y-m-d"))->where('income_date','<=',$lastDay->format("Y-m-d"))->sum('price');
+        $price_month = $sale_month + $income_month;
+        
         //月支出
         $pay_month = PayData::where('pay_date','>=',$firstDay->format("Y-m-d"))->where('pay_date','<=',$lastDay->format("Y-m-d"))->sum('price');
         $income = IncomeData::where('income_date',$today->format("Y-m-d"))->sum('price');
