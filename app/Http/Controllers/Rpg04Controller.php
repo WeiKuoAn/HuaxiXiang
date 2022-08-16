d5d75dc42a324f60559313087099601353c700ba
WeiKuoAn <qaz898970@gmail.com>
2022-08-16 21:14:20 +0800

    0816新增金紙報表

--- app/Http/Controllers/Rpg04Controller.php
+++ app/Http/Controllers/Rpg04Controller.php
@@ -21,6 +21,7 @@ class Rpg04Controller extends Controller
         $after_date = Carbon::now()->firstOfMonth();
         $before_date = Carbon::now()->lastOfMonth();
         $periods = CarbonPeriod::create($after_date, $before_date);
+        
        
 
         $gdpapers = Gdpaper::where('status','up')->get();
@@ -39,7 +40,9 @@ class Rpg04Controller extends Controller
             if($after_date && $before_date){
                 $gdpaper_datas = Sale_gdpaper::where('created_at','>=',$after_date)->where('created_at','<=',$before_date)->get();
             }
-            $periods = CarbonPeriod::create($after_date, $before_date);
+            if($after_date && $before_date){
+                $periods = CarbonPeriod::create( $request->after_date,  $request->before_date);
+            }
         }
 
         $datas = [];
